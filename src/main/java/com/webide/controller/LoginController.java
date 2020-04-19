bc9e6ab465c2a1fd5113a36ad28f3a2f8f5376fe
FreedomJackXuan <1024946411@qq.com>
2020-04-19 20:01:06 +0800

    update

--- src/main/java/com/webide/controller/LoginController.java
+++ src/main/java/com/webide/controller/LoginController.java
@@ -30,7 +30,7 @@ public class LoginController {
 
     @RequestMapping("/do_login")
     @ResponseBody
-    public Result<String> doLogin(HttpServletResponse response, @RequestBody @Valid LoginVo loginVo) {//加入JSR303参数校验
+    public Result<String> doLogin(HttpServletResponse response, @RequestBody @Valid LoginVo loginVo) {
         log.info(loginVo.toString());
         String token = userService.login(response, loginVo);
         return Result.success(token);
@@ -38,7 +38,7 @@ public class LoginController {
 
     @RequestMapping("/token")
     @ResponseBody
-    public Result<String> tokenLogin(NativeWebRequest nativeWebRequest) {//加入JSR303参数校验
+    public Result<String> tokenLogin(NativeWebRequest nativeWebRequest) {
         String token = userService.tokenLogin(nativeWebRequest);
         return Result.success(token);
     }
